5836970c73bc7e7f6686ebff75516be3651db6af
TheRedBrain <thetrueredbrain@gmail.com>
2024-10-19 14:27:57 +0200

    setup and renamed to various items

--- src/client/java/com/github/theredbrain/variousitems/VariousItemsClient.java
+++ src/client/java/com/github/theredbrain/variousitems/VariousItemsClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.github.theredbrain.variousitems;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class VariousItemsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
